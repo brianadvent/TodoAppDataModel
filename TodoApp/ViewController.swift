1c3dc43c44ed8decbd6748f4c2034c7fd18a5c05
Brian Advent <youtube@brianadvent.com>
2017-12-10 15:49:22 +0100

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Brian Advent on 10.12.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
