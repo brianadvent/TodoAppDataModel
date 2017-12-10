65a139bfa3f59fad503f00b7b513624fe184eff8
Brian Advent <youtube@brianadvent.com>
2017-12-10 19:35:48 +0100

    DataModel

---
+++ TodoApp/ToDoItem.swift
@@ -0,0 +1,31 @@
+//
+//  ToDoItem.swift
+//  TodoApp
+//
+//  Created by Brian Advent on 10.12.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import Foundation
+
+struct TodoItem : Codable {
+    
+    var title:String
+    var completed:Bool
+    var createdAt:Date
+    var itemIdentifier:UUID
+    
+    func saveItem() {
+        DataManager.save(self, with: itemIdentifier.uuidString)
+    }
+    
+    func deleteItem() {
+        DataManager.delete(itemIdentifier.uuidString)
+    }
+    
+    mutating func markAsCompleted() {
+        self.completed = true
+        DataManager.save(self, with: itemIdentifier.uuidString)
+    }
+    
+}
